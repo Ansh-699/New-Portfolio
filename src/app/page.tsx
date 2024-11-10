80ed810933140374e432b99b9dc1baf781814a42
templeajuzie <templeajuzie@gmail.com>
2024-11-10 14:44:49 +0100

    feat: new update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,10 +14,10 @@ const BLUR_FADE_DELAY = 0.04;
 export default function Page() {
   return (
     <main className="flex flex-col min-h-[100dvh] space-y-10">
-      <section id="hero">
+      <section id="hero" className="mt-20 lg:max-lg:t-0">
         <div className="mx-auto w-full max-w-2xl space-y-8">
           <div className="gap-2 flex justify-between">
-            <div className="flex-col flex flex-1 space-y-1.5 mt-20 lg:mt-10">
+            <div className="flex-col flex flex-1 space-y-1.5 mt-10">
               <BlurFadeText
                 delay={BLUR_FADE_DELAY}
                 className="text-3xl font-bold tracking-tighter sm:text-2xl xl:text-3xl/none"
